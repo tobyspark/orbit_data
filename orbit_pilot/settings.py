605ba8be0c06a85d058c48e257c6d49b2eaf6466
Toby Harris <operator@tobyz.net>
2019-12-05 00:49:00 +0000

    Production settings via .env

--- orbit_pilot/settings.py
+++ orbit_pilot/settings.py
@@ -16,19 +16,14 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qbc@!=8s*r$ggvvq)k=^7-4u$x_zb-^(ry6u2&6lzyy)b$^!@j'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-if DEBUG:
-    ALLOWED_HOSTS = ['*'] # NOTE: Set to allow all hosts to enable access from iOS devices on network.
+if 'ORBIT_PRODUCTION' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['HOST']]
 else:
-    ALLOWED_HOSTS = []
+    DEBUG = True
+    SECRET_KEY = 'OARITRKGWJts_bmdf7FjEcSmLsyfKB_T7reZTChC_GM'
+    ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -125,5 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.environ['STATIC_ROOT']
 
-MEDIA_ROOT = os.environ['CityPilotMediaRoot']
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.environ['MEDIA_ROOT']
