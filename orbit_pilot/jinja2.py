d210a17066b5061aea40270d8124d822a3d68b1b
Toby Harris <operator@tobyz.net>
2019-12-06 13:51:14 +0000

    HTML: Better structure

--- orbit_pilot/jinja2.py
+++ orbit_pilot/jinja2.py
@@ -1,13 +1,14 @@
-from django.templatetags.static import static
-from django.urls import reverse
-
 from jinja2 import Environment
 
+from django.templatetags.static import static
+from django.urls import reverse
+from django.core.exceptions import NON_FIELD_ERRORS
 
 def environment(**options):
     env = Environment(**options)
     env.globals.update({
         'static': static,
         'url': reverse,
+        'NON_FIELD_ERRORS': NON_FIELD_ERRORS
     })
     return env
