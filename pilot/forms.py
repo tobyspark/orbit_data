addc1b0063d3edcd90477d8e44f6849e595e39d5
Toby Harris <operator@tobyz.net>
2019-12-13 10:49:45 +0000

    GenderField

--- pilot/forms.py
+++ pilot/forms.py
@@ -8,6 +8,58 @@ def validate_age(value):
     if value > 100: raise ValidationError('Age too old')
     if value % 5 != 0: raise ValidationError('Age not rounded to nearest five-years')
 
+class GenderWidget(forms.MultiWidget):
+    def __init__(self, attrs={}):
+        widgets = (
+            forms.RadioSelect(
+                choices=[
+                    ('M', 'Male'),
+                    ('F', 'Female'),
+                    ],
+                attrs=attrs,
+                ),
+            forms.TextInput(
+                attrs=attrs.update({'placeholder': 'Or, how do you identify?'}),
+                ),
+        )
+        super().__init__(widgets, attrs)
+    
+    def decompress(self, value):
+        if value:
+            if value in ['M', 'F']:
+                return [value, '']
+            return ['', value]
+        return ['', '']
+
+class GenderField(forms.MultiValueField):
+    def __init__(self, **kwargs):
+        widget = GenderWidget()
+        fields = (
+            forms.ChoiceField(
+                required=False,
+                label='',
+                choices=[
+                    ('M', 'Male'),
+                    ('F', 'Female'),
+                    ],
+                ),
+            forms.CharField(
+                required=False,
+                label='',
+                min_length=1,
+                max_length=128,
+                ),
+        )
+        super().__init__(widget=widget, fields=fields, required=False, **kwargs)
+    
+    def compress(self, data_list):
+        if data_list:
+            if data_list[0] in ['M', 'F'] and data_list[1] is '':
+                return data_list[0]
+            if data_list[0] == '' and len(data_list[1]) > 0:
+                return data_list[1]
+        return None
+
 @parsleyfy
 class ConsentForm(BetterForm):
     class Meta:
@@ -88,8 +140,7 @@ class SurveyForm(BetterForm):
                     ]}),
             ('Gender', {
                 'fields': [
-                    'genderChoice',
-                    'genderIdentify',
+                    'gender',
                     ]}),
             ('Vision', {
                 'fields': [
@@ -152,23 +203,8 @@ class SurveyForm(BetterForm):
             'placeholder': '20, 25, 30, ...',
             }),
         )
-    genderChoice = forms.ChoiceField(
-        required=False,
+    gender = GenderField(
         label='',
-        choices=[
-            ('M', 'Male'),
-            ('F', 'Female'),
-            ],
-        widget=forms.RadioSelect,
-        )
-    genderIdentify = forms.CharField(
-        required=False,
-        label='',
-        min_length=1,
-        max_length=128,
-        widget=forms.TextInput(attrs={
-            'placeholder': 'Or, how do you identify?'
-            }),
         )
     vision_light_perception = forms.ChoiceField(
         label='',
