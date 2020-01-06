70d98457fb6e0c469e3b0e589158913b0ab5f949
Toby Harris <operator@tobyz.net>
2020-01-06 18:19:28 +0000

    Participant is encrypted

--- pilot/admin.py
+++ pilot/admin.py
@@ -6,10 +6,25 @@ from .models import LabelledMedia, Participant, Survey
 
 admin.site.register(LabelledMedia)
 
-admin.site.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    '''
+    Provides an export action that will decrypt the participant name and email address and return as JSON
+    '''
+    actions = ['export']
+ 
+    def export(self, request, queryset):
+        data = { item.id: item.decrypt() for item in queryset }
+        return HttpResponse(
+            json.dumps(data),
+            content_type="application/json",
+            )
+
+admin.site.register(Participant, ParticipantAdmin)
 
 class SurveyAdmin(admin.ModelAdmin):
-    
+    '''
+    Provides an export action that will decrypt the survey data and return as JSON
+    '''
     actions = ['export']
     
     def export(self, request, queryset):
--- pilot/models.py
+++ pilot/models.py
@@ -15,17 +15,104 @@ def mint_token():
     return secrets.token_urlsafe(TOKEN_BYTES)
 def mint_id():
     return secrets.randbelow(10000)
+
+def encrypt(data):
+    '''
+    Encrypt data, returning dict of encryption info. As data to encrypt is of an unknown size, strategy is to use AES to encrypt the data, and RSA to encrypt the AES key.
+    '''
+    # Get keys
+    public_key = RSA.import_key(settings.PII_KEY_PUBLIC)
+    aes_key = secrets.token_bytes(32) # AES-256, i.e. 256/8=32
+    
+    # Encrypt the AES 'session' key with the public RSA key
+    cipher_rsa = PKCS1_OAEP.new(public_key)
+    enc_aes_key = cipher_rsa.encrypt(aes_key)
+    
+    # Encrypt the data using AES
+    cipher_aes = AES.new(aes_key, AES.MODE_EAX)
+    ciphertext, mac_tag = cipher_aes.encrypt_and_digest(data)
+    
+    return {
+        'enc_aes_key': enc_aes_key,
+        'aes_nonce': cipher_aes.nonce,
+        'aes_mac_tag': mac_tag,
+        'aes_ciphertext': ciphertext,
+    }
+    
+def decrypt(enc_aes_key, aes_nonce, aes_mac_tag, aes_ciphertext):
+    '''
+    Decrypt data, returning bytes.
+    '''
+    private_key_pem = settings.PII_KEY_PRIVATE
+    if private_key_pem is None:
+        print('Attempting to decrypt without private key')
+        return None
         
-class Participant(models.Model):
+    private_key = RSA.import_key(private_key_pem)
+    
+    # Decrypt the AES 'session' key with the private RSA key
+    cipher_rsa = PKCS1_OAEP.new(private_key)
+    aes_key = cipher_rsa.decrypt(enc_aes_key)
+    
+    # Decrypt the data with the AES session key
+    cipher_aes = AES.new(aes_key, AES.MODE_EAX, aes_nonce)
+    data = cipher_aes.decrypt_and_verify(aes_ciphertext, aes_mac_tag)
+    
+    return data
+
+
+class EncryptedBlobModel(models.Model):
+    '''
+    Abstract base class for models that hold an encrypted blob of data
+    '''
+    class Meta:
+        abstract = True
+        
+    enc_aes_key = models.BinaryField(max_length=16)
+    aes_nonce = models.BinaryField(max_length=16)
+    aes_mac_tag = models.BinaryField(max_length=16)
+    aes_ciphertext = models.BinaryField()
+    
+    def decrypt(self):
+        data = decrypt(
+            self.enc_aes_key,
+            self.aes_nonce,
+            self.aes_mac_tag,
+            self.aes_ciphertext
+            )
+        
+        if data is None:
+            return None
+        
+        # Transform back into fields
+        obj = json.loads(data.decode("utf-8"))
+        
+        return obj
+
+
+class ParticipantManager(models.Manager):
+    def create_participant(self, email, name):
+        '''
+        Create and save a participant record, encrypting the supplied fields as a binary blob.
+        '''
+        # Transform email and name into bytes
+        info = {
+            'email': email,
+            'name': name,
+        }
+        data = json.dumps(info).encode('utf-8')
+        
+        # Encrypt and create
+        return self.create(**encrypt(data))
+
+
+class Participant(EncryptedBlobModel):
+    '''
+    A study participant, with human-usable randomised ID and email, name in encrypted blob.
+    '''
     id = models.IntegerField(
         primary_key=True,
         default=mint_id,
-        )
-    email = models.EmailField(
-        unique=True,
-        )
-    name = models.CharField(
-        max_length=128,
         )
     survey_started = models.DateTimeField(
         null=True,
@@ -36,78 +123,39 @@ class Participant(models.Model):
         unique=True,
         )
     
+    objects = ParticipantManager()
+    
     @property
     def survey_done(self):
         return Survey.objects.filter(participant=self).exists()
-    
 
 
 class SurveyManager(models.Manager):
     def create_survey(self, participant, fields):
         '''
-        Create and save a survey entry, encrypting the supplied fields as a binary blob.
-        As data to encrypt is of an unknown size, strategy is to use AES to encrypt the data, and RSA to encrypt the AES key.
+        Create and save a survey record, encrypting the supplied fields as a binary blob.
         '''
-        
         # Transform survey data (a dict) into bytes
         data = json.dumps(fields).encode('utf-8')
         
-        # Get keys
-        public_key = RSA.import_key(settings.PII_KEY_PUBLIC)
-        aes_key = secrets.token_bytes(32) # AES-256, i.e. 256/8=32
-        
-        # Encrypt the AES 'session' key with the public RSA key
-        cipher_rsa = PKCS1_OAEP.new(public_key)
-        enc_aes_key = cipher_rsa.encrypt(aes_key)
-        
-        # Encrypt the data using AES
-        cipher_aes = AES.new(aes_key, AES.MODE_EAX)
-        ciphertext, mac_tag = cipher_aes.encrypt_and_digest(data)
-        
-        survey = self.create(
+        # Encrypt and create
+        return self.create(
             participant=participant,
-            enc_aes_key = enc_aes_key,
-            aes_nonce = cipher_aes.nonce,
-            aes_mac_tag = mac_tag,
-            aes_ciphertext = ciphertext,
+            **encrypt(data),
             )
         
 
-class Survey(models.Model):
+class Survey(EncryptedBlobModel):
     participant = models.ForeignKey(
         Participant,
         on_delete=models.CASCADE,
         )
-    enc_aes_key = models.BinaryField(max_length=16)
-    aes_nonce = models.BinaryField(max_length=16)
-    aes_mac_tag = models.BinaryField(max_length=16)
-    aes_ciphertext = models.BinaryField()
     
     objects = SurveyManager()
     
     def __str__(self):
         return f"Survey for Participant {self.participant.id}"
-        
-    def decrypt(self):
-        private_key_pem = settings.PII_KEY_PRIVATE
-        if private_key_pem is None:
-            print('Attempting to decrypt without private key')
-            return None
-            
-        private_key = RSA.import_key(private_key_pem)
-        
-        # Decrypt the AES 'session' key with the private RSA key
-        cipher_rsa = PKCS1_OAEP.new(private_key)
-        aes_key = cipher_rsa.decrypt(self.enc_aes_key)
-        
-        # Decrypt the data with the AES session key
-        cipher_aes = AES.new(aes_key, AES.MODE_EAX, self.aes_nonce)
-        data = cipher_aes.decrypt_and_verify(self.aes_ciphertext, self.aes_mac_tag)
-        
-        # Transform back into fields
-        fields = json.loads(data.decode("utf-8"))
-        
-        return fields
+
 
 class LabelledMedia(models.Model):
     label = models.CharField(max_length=50)
