8341fc54b162568bc7f7d8cbe0e619b640d28296
Toby Harris <operator@tobyz.net>
2020-01-20 21:02:58 +0000

    Admin refactor export

--- pilot/admin.py
+++ pilot/admin.py
@@ -10,20 +10,21 @@ class ParticipantAdmin(admin.ModelAdmin):
     '''
     Provides an export action that will decrypt the participant name and email address and return as JSON
     '''
-    actions = ['export']
- 
-    def export(self, request, queryset):
-        data = {
+    actions = ['export_json']
+    
+    def data(self, queryset):
+        return {
             item.id: {
                 **item.decrypt(),
                 'publishing_videos': item.publishing_videos,
                 'publishing_recordings': item.publishing_recordings,
                 'publishing_quotes': item.publishing_quotes,
                 } for item in queryset
-        }
-        
+            }
+    
+    def export_json(self, request, queryset):
         return HttpResponse(
-            json.dumps(data),
+            json.dumps(self.data(queryset)),
             content_type="application/json",
             )
 
@@ -33,12 +34,14 @@ class SurveyAdmin(admin.ModelAdmin):
     '''
     Provides an export action that will decrypt the survey data and return as JSON
     '''
-    actions = ['export']
+    actions = ['export_json']
     
-    def export(self, request, queryset):
-        data = { item.participant.id: item.decrypt() for item in queryset }
+    def data(self, queryset):
+        return { item.participant.id: item.decrypt() for item in queryset }
+    
+    def export_json(self, request, queryset):
         return HttpResponse(
-            json.dumps(data),
+            json.dumps(self.data(queryset)),
             content_type="application/json",
             )
 
