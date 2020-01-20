eaf6f13e016433a8d4474caa42ef2cdd59d9a89a
Toby Harris <operator@tobyz.net>
2020-01-20 17:51:09 +0000

    PrimaryKeyRelatedField

--- pilot/serializers.py
+++ pilot/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 from .models import LabelledMedia
 
 class LabelledMediaSerializer(serializers.HyperlinkedModelSerializer):
+    participant = serializers.PrimaryKeyRelatedField(read_only=True)
+    
     class Meta:
         model = LabelledMedia
         fields = ['label', 'media', 'participant']
