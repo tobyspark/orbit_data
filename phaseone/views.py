88eead8f04a662cd72e29136133f1c428b4e0cf0
Toby Harris <operator@tobyz.net>
2020-04-29 23:01:39 +0100

    API has createparticipant endpoint

--- phaseone/views.py
+++ phaseone/views.py
@@ -5,10 +5,13 @@ from django.db import IntegrityError
 from django.utils.timezone import now
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.generics import CreateAPIView
+from secrets import token_hex
+from base64 import b64encode
 
 from .forms import SurveyForm
 from .models import Participant, Survey, Thing, Video
-from .serializers import ThingSerializer, VideoSerializer
+from .serializers import ParticipantCreateSerializer, ThingSerializer, VideoSerializer
 
 def survey(request, token):
     """
@@ -53,14 +56,40 @@ def survey_done(request):
     Static, no further navigation.
     """
     return render(request, 'phaseone/survey_done.html')
-    
+
+
+class ParticipantCreateView(CreateAPIView):
+    """
+    API endpoint to create a new user+participant
+    """
+    serializer_class = ParticipantCreateSerializer
+
+# TODO: PERMISSION: Only OrbitCamera user
+
+    def perform_create(self, serializer):
+        # Create random username and password
+        username = token_hex(16)
+        password = token_hex(16)
+        user = User.objects.create_user(username, password=password)
+
+        # Create participant
+        pii_email = serializer.validated_data['email']
+        pii_name = serializer.validated_data['name']
+        Participant.objects.create_participant(user, pii_email, pii_name)
+
+        # Create the auth credential to return as API response
+        token_bytes = f'{username}:{password}'.encode()
+        serializer.validated_data['auth_credential'] = 'Basic ' + b64encode(token_bytes).decode()
+
 class ThingViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows Things to be viewed or edited
     """
     queryset = Thing.objects.all().order_by('-created')
     serializer_class = ThingSerializer
-    
+
+# TODO: get query to filter to participant things
+
     def perform_create(self, serializer):
         participant = Participant.objects.get(id=self.request.user.username)
         serializer.save(participant=participant)
@@ -71,3 +100,5 @@ class VideoViewSet(viewsets.ModelViewSet):
     """
     queryset = Video.objects.all().order_by('-created')
     serializer_class = VideoSerializer
+
+# TODO: get query to filter to participant things
