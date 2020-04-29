311769743bfea72db82f8fad32f3a8f076a482cf
Toby Harris <operator@tobyz.net>
2020-04-29 18:13:30 +0100

    Remove Participant Info, Consent.

--- phaseone/urls.py
+++ phaseone/urls.py
@@ -8,8 +8,6 @@ router.register(r'thing', views.ThingViewSet)
 router.register(r'video', views.VideoViewSet)
 
 urlpatterns = [
-    path('', views.info, name='info'),
-    path('consent', views.consent, name='consent'),
     path('survey/done', views.survey_done, name='survey_done'),
     path('survey/<token>', views.survey, name='survey'),
     path('api/', include(router.urls))
