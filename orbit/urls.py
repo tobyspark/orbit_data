c1c07f2279797e005069bf8ba5bc5816d65dcea9
Toby Harris <operator@tobyz.net>
2020-05-01 00:39:31 +0100

    URLs – Remove pilot

--- orbit/urls.py
+++ orbit/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('pilot/', include('pilot.urls')),
+    # path('pilot/', include('pilot.urls')), # Pilot now closed
     path('phaseone/', include('phaseone.urls')),
     path('admin/', admin.site.urls),
 ]
