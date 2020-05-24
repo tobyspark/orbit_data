fc5867f098189405d7fd8395e895729656e23053
Toby Harris <operator@tobyz.net>
2020-05-24 15:45:06 +0100

    Participant endpoint

--- phaseone/urls.py
+++ phaseone/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('survey/done', views.survey_done, name='survey_done'),
     path('survey/<token>', views.survey, name='survey'),
     path('api/', include(router.urls)),
+    path('api/participant/', views.ParticipantView.as_view()),
     path('api/createparticipant/', views.ParticipantCreateView.as_view()),
     path('participant_export/', views.participant_export, name='participant_export'),
 ]
