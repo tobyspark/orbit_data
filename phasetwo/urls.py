21f1bc69d5fb748eb6d47f15ad4a2701270fe029
Toby Harris <operator@tobyz.net>
2020-10-25 22:18:54 +0000

    APNS create_apns_device

--- phasetwo/urls.py
+++ phasetwo/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
+from push_notifications.api.rest_framework import APNSDeviceAuthorizedViewSet
 
 from . import views
 
@@ -14,4 +15,5 @@ urlpatterns = [
     path('api/participant/', views.ParticipantView.as_view()),
     path('api/createparticipant/', views.ParticipantCreateView.as_view()),
     path('participant_export/', views.participant_export, name='participant_export'),
+    path('device/apns/', APNSDeviceAuthorizedViewSet.as_view({'post': 'create'}), name='create_apns_device'),
 ]
